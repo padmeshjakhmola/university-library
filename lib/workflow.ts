57494f72ae35c0282cffedf52abaccfce2099778
Padmesh Jakhmola <prankshadow@gmail.com>
2025-02-09 14:32:58 +0530

    implement qstash workflow and resend

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,35 @@
 import { Client as WorkFlowClient } from "@upstash/workflow";
 import config from "@/lib/config";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkFlowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "University Library <hello.universitylibrary.store>",
+      to: [email],
+      subject: subject,
+      html: message,
+    },
+  });
+};
