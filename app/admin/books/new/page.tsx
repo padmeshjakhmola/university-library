88ec74eda42a38c4c7245d7cb14ec52537650abb
Padmesh Jakhmola <prankshadow@gmail.com>
2025-02-13 19:39:54 +0530

    created admin pannel

---
+++ app/admin/books/new/page.tsx
@@ -0,0 +1,21 @@
+import BookForm from "@/components/admin/forms/BookForm";
+import { Button } from "@/components/ui/button";
+import { Book } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <Button asChild className="back-btn">
+        <Link href="/admin/books">Go Back</Link>
+      </Button>
+
+      <section className="w-full max-w-2xl">
+        <BookForm />
+      </section>
+    </>
+  );
+};
+
+export default Page;
