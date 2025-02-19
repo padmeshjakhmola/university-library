1085b48e9f7e949f03f86fd412f40b3a1e0ad297
Padmesh Jakhmola <prankshadow@gmail.com>
2025-02-19 13:24:19 +0530

    added logout woth avatar

---
+++ components/AvatarImageComponent.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+
+const AvatarImageComponent = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut({ redirect: false });
+    router.push("/sign-in");
+  };
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="cursor-pointer">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="bg-amber-100">
+              {/* {getInitials(session?.user?.name || "IN")} */}
+              <p>UL</p>
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-auto">
+          <DropdownMenuItem
+            onClick={handleLogout}
+            className="justify-between text-[#ef4444]  hover:bg-transparent cursor-pointer"
+          >
+            Logout
+            <Image
+              src="/icons/logout.svg"
+              width={20}
+              height={20}
+              alt="logout"
+            />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default AvatarImageComponent;
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,31 +8,17 @@ import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import AvatarImageComponent from "./AvatarImageComponent";
 
 const Header = () => {
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    await signOut({ redirect: false });
-    router.push("/sign-in");
-  };
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
         <Image src="./icons/logo.svg" alt="logo" width={40} height={40} />
       </Link>
-      <ul className="flex flex-row items-center gap-8">
-        {/* <li> */}
-          {/* <Link href="/my-profile">
-            <Avatar>
-              <AvatarFallback className="bg-amber-100">
-                {getInitials(session?.user?.name || "IN")}
-              </AvatarFallback>
-            </Avatar>
-          </Link> */}
-          <Button onClick={handleLogout}>Logout</Button>
-        {/* </li> */}
-      </ul>
+      <div className="flex flex-row items-center gap-8">
+        <AvatarImageComponent />
+      </div>
     </header>
   );
 };
